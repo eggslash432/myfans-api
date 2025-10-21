2c4a77c7f1b9f57bd5ab10566a98690ebdab596f
ティルエッグ <greateggsandwich7@gmail.com>
2025-10-21 19:57:39 +0900

    記事をリロードするとjsonが直に表示される問題を修正

--- src/apps/core/main.ts
+++ src/apps/core/main.ts
@@ -9,6 +9,8 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   // バリデーション
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }),
